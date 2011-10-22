e4cbc92fb8d00fd7d5dcb846b79dc4f697e0fb87
Graham Rich <richgr@gmail.com>
2011-10-22 00:59:19 -0300

    fixed the secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
-
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
-Eighth::Application.config.secret_token = '719eeca9eca6a1ef7b5aea381a541568fed49b9fd72a834d0de27353d605da9fb3745ee7547a73473794638d3cab690e9a038cf9ab07ccfe9838d75f643e38bb'
-=======
 Eighth::Application.config.secret_token = 'af821368191a0b83bc683d46ded4c46485f58596b918a4620e1d3a52bb92cf44ca15c3a7d30c254bc8d30a9327d0fd2091d30621ecdd4699821d6d1904971b37'
->>>>>>> b4d3ead846026a99c50214ffa482d638d4656818
