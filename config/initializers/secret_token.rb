b4d3ead846026a99c50214ffa482d638d4656818
Graham Rich <richgr@gmail.com>
2011-10-21 23:39:39 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eighth::Application.config.secret_token = 'af821368191a0b83bc683d46ded4c46485f58596b918a4620e1d3a52bb92cf44ca15c3a7d30c254bc8d30a9327d0fd2091d30621ecdd4699821d6d1904971b37'
