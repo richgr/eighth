624a7506b055ef087fa6e388d85764b7874bfb73
Graham Rich <richgr@gmail.com>
2011-10-22 00:42:42 -0300

    routes and database fixed (?)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Eighth::Application.routes.draw do
 
-  root :to => 'pages#home'
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  get "pages/help"
 
-  match '/contact', :to => 'pages#contact'
-  match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
+#
+#  root :to => 'pages#home'
+#
+#  match '/contact', :to => 'pages#contact'
+#  match '/about',   :to => 'pages#about'
+#  match '/help',    :to => 'pages#help'
 
 end
