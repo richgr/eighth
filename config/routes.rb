7735f57774ec75f39d97b1b5797bd36f0f83e24c
Graham Rich <richgr@gmail.com>
2011-10-22 01:43:01 -0300

    Heroku fail 040: trying to undo pages complexity

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Eighth::Application.routes.draw do
 
-#  get "pages/home"
-#  get "pages/contact"
-#  get "pages/about"
-#  get "pages/help"
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  get "pages/help"
 
 
-  root :to => 'pages#home'
-
-  match '/contact', :to => 'pages#contact'
-  match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
+#  root :to => 'pages#home'
+#
+#  match '/contact', :to => 'pages#contact'
+#  match '/about',   :to => 'pages#about'
+#  match '/help',    :to => 'pages#help'
 
 end
