6e53cd9946ecc0747b863ce41b99ce7d0ca716fe
Graham Rich <richgr@gmail.com>
2011-10-22 13:28:05 -0300

    Heroku fail 054: config/routes change over in style 02

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Eighth::Application.routes.draw do
 
 #  get "pages/home"
-  get "pages/contact"
+#  get "pages/contact"
   get "pages/about"
   get "pages/help"
 
 
   root :to => 'pages#home'
-#
-#  match '/contact', :to => 'pages#contact'
+
+  match '/contact', :to => 'pages#contact'
 #  match '/about',   :to => 'pages#about'
 #  match '/help',    :to => 'pages#help'
 
