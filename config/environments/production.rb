2d03871a46afe34bf79072cf9206b000375ade39
Graham Rich <richgr@gmail.com>
2011-10-24 11:05:03 -0300

    Heroku solution 125?  found 'blueprint screen css' solition on StackOverflow, edited config/env/prod.rb, moved stylesheets back to proper locations, re-run rake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Eighth::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( blueprint/ie.css blueprint/screen.css	blueprint/print.css custom.css scaffold.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
