51fd03bde51a0fa3721727626ddbd10aa7a14ae1
Graham Rich <richgr@gmail.com>
2011-10-25 11:15:28 -0300

    Added tests for user_path(@user), etc.  These pull 'first' from the db.  There'd better be an entry in the test db.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,4 +10,31 @@ describe "Users" do
 #    end
 #  end
 
+
+  # Retrieve the first valid user from the database
+  # BE SURE your test database has at least one user! ####
+  before(:each) do
+    @user = User.first
+  end
+
+  it "should visit a valid users_path" do
+    visit users_path
+    response.should have_selector('title', :content => "Users")
+  end
+ 
+  it "should visit a valid user_path(@user)" do
+    visit user_path(@user)
+    response.should have_selector('title', :content => "Show")
+  end
+  
+  it "should visit a valid new_user_path" do
+    visit new_user_path
+    response.should have_selector('title', :content => "Sign up")
+  end
+  
+  it "should visit a valid edit_user_path(@user)" do
+    visit edit_user_path(@user)
+    response.should have_selector('title', :content => "Edit")
+  end
+
 end
