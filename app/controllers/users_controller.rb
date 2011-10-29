36273d68513e56bdeabf3845828fb6d870a1c087
Graham Rich <richgr@gmail.com>
2011-10-28 22:08:46 -0300

    Faker working, will_paginate working in basic mode (but we don't know why or how to change it yet)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(:page => params[:page])
     @title = "All users"
   end
 
