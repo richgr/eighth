f20ba840ba452257cf55471ee40ec9ffcd5ca38b
Graham Rich <richgr@gmail.com>
2011-10-29 22:54:03 -0300

    Can now delete users (as admins, but no destroying self)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
 require 'spec_helper'
@@ -166,27 +167,52 @@ describe User do
       end
     end
 
-#    describe "authenticate method" do
-#
-#      it "should exist" do
-#        User.should respond_to(:authenticate)
-#      end
-#
-#      it "should return nil on email/password mismatch" do
-#        User.authenticate(@attr[:email], "wrong_password").should be_nil
-#      end
-#
-#      it "should return nil on email address with no user" do
-#        User.authenticate("bar@foo.com", @attr[:password]).should be_nil
-#      end
-#
-#      it "should return the user on email/password match" do
-#        User.authenticate(@attr[:email], @attr[:password]).should == @user
-#      end
-#
-#    end
+    describe "authenticate method" do
+
+      it "should exist" do
+        User.should respond_to(:authenticate)
+      end
+
+      it "should return nil on email/password mismatch" do
+        User.authenticate(@attr[:email], "wrong_password").should be_nil
+      end
+
+      it "should return nil on email address with no user" do
+        User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+      end
+
+      it "should return the user on email/password match" do
+        User.authenticate(@attr[:email], @attr[:password]).should == @user
+      end
+ 
+    end
 
   end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+    
+    
+    
+  end
+  
 #
 #  # original file contents
 #  # pending "add some examples to (or delete) #{__FILE__}"
